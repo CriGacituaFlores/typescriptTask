076a76f71375fa9961cf678ed87eb5db28bfcb7d
cristian gacitua <cgacitua@acid.cl>
2020-01-16 17:59:21 -0300

    enum

--- app.ts
+++ app.ts
@@ -1,13 +1,23 @@
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 1;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR
+}
+
 const person: {
-  name: string,
-  age: number,
-  hobbies: string[],
-  role: [number, string]
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: number;
 } = {
   name: "Pepito",
   age: 30,
   hobbies: ["Sport", "Cooking"],
-  role: [2, 'author']
+  role: Role.ADMIN
 };
 
 let favoriteActivities: string[];
@@ -15,4 +25,8 @@ favoriteActivities = ["Sports"];
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toLocaleUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
 }
